a8296f8cd5b7aab22e40da5cdc049413bd43dccf
billwillman <billwillman@qq.com>
2020-03-02 20:39:54 +0800

    1

---
+++ Project/Assets/Scritps/TestJob.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Jobs;
+using UnityEngine.LowLevel;
+
+
+using Unity;
+using Unity.Jobs;
+using Unity.Jobs.LowLevel;
+using Unity.Collections;
+using Unity.Collections.LowLevel;
+using Unity.Mathematics;
+using Unity.IO;
+using Unity.IO.LowLevel;
+using Unity.Jobs.LowLevel.Unsafe;
+//using Unity.Entities;
+
+
+public struct TestJob 
+{
+}
